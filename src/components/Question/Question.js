aa7940afd823c68103d2014125039a1315f5f0b7
Md.Al kafee <sojeebahmed.sa@gmail.com>
2022-10-12 02:15:44 +0600

    added eye icon and show the correct answer

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,20 +1,34 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Options from '../Options/Options';
+import { faEye } from '@fortawesome/free-regular-svg-icons';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 
 const Question = ({ questionSet }) => {
-    const { id, name, options, question, correctAnswer } = questionSet;
+   
+ const  answer =()=>{ toast(correctAnswer)};
+
+    const { options, question, correctAnswer } = questionSet;
     // console.log(questionSet);
     return (
-        <div className='border-2 shadow-lg shadow-teal-400 m-auto w-3/6 my-10 p-5 rounded-lg'>
+        <div className=' border-2 shadow-lg shadow-teal-400 m-auto w-3/6 my-10 p-5 rounded-lg'>
 
-            <h2 className='mb-10 text-teal-600 text-xl font-semibold'>{question}
+            <div className='flex justify-center item-center'>
+            <h2 className='mb-10 text-teal-600 text-xl font-semibold'>
+                {question}
             </h2>
-            
+            <FontAwesomeIcon onClick={answer} className='mt-2 ml-3'  icon={faEye}></FontAwesomeIcon>
+            <ToastContainer 
+             theme="light" position="top-right" autoClose={3000}
+             ></ToastContainer>
+            </div>
+
 
             {
-                options.map((option,idx) => <Options
+                options.map((option, idx) => <Options
                     key={idx}
                     option={option}
                     correctAnswer={correctAnswer}
