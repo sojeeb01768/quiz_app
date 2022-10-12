51bd984703cc89a1b4f55fac7f021a5685e1def7
Md.Al kafee <sojeebahmed.sa@gmail.com>
2022-10-12 13:06:26 +0600

    added some style in home page

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -8,22 +8,22 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 const Question = ({ questionSet }) => {
-   
- const  answer =()=>{ toast(correctAnswer)};
+
+    const answer = () => { toast(correctAnswer) };
 
     const { options, question, correctAnswer } = questionSet;
     // console.log(questionSet);
     return (
-        <div className=' border-2 shadow-lg shadow-teal-400 m-auto w-3/6 my-10 p-5 rounded-lg'>
+        <div className=' border-2 shadow-lg shadow-teal-400 my-10 m-auto w-4/6 sm:w-4/6  p-5 rounded-lg'>
 
             <div className='flex justify-center item-center'>
-            <h2 className='mb-10 text-teal-600 text-xl font-semibold'>
-                {question}
-            </h2>
-            <FontAwesomeIcon onClick={answer} className='mt-2 ml-3'  icon={faEye}></FontAwesomeIcon>
-            <ToastContainer 
-             theme="light" position="top-left" autoClose={3000}
-             ></ToastContainer>
+                <h2 className='mb-10 text-teal-600 text-xl font-semibold'>
+                    {question}
+                </h2>
+                <FontAwesomeIcon onClick={answer} className='mt-2 ml-3' icon={faEye}></FontAwesomeIcon>
+                <ToastContainer
+                    theme="light" position="top-left" autoClose={3000}
+                ></ToastContainer>
             </div>
 
 
--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -6,25 +6,31 @@ import './Topics.css';
 const Topics = () => {
     const topics = useLoaderData();
     // console.log(topics.data);
-    
+
     return (
-        
+
         <div >
-            <div className='m-auto  my-5 p-5 shadow-lg'>
-            <h2 className='text-3xl my-2 font-semibold text-teal-600'>The Best Quiz Maker for Learning & Education</h2>
-            <p className='m-auto px-10'>
-                Test YourSelf is secure, professional web-based Quiz maker is an easy-to-use, customizable online testing solution for business, training & educational assessments with Tests & Quizzes graded instantly, saving hours of paperwork!
-            </p>
+            <div className='m-auto  my-5 p-5 shadow-lg flex bg-gray-300 mx-5 px-50 rounded-lg'>
+
+                <img className='w-2/6 h-3/6 rounded-lg' src="https://leverageedublog.s3.ap-south-1.amazonaws.com/blog/wp-content/uploads/2020/03/24185535/Online-Learning.jpg" alt="" />
+
+
+                <div>
+                    <h2 className='text-3xl my-2 font-semibold text-teal-600'>The Best Quiz Maker for Learning & Education</h2>
+                    <p className='m-auto px-10'>
+                        Test YourSelf is secure, professional web-based Quiz maker is an easy-to-use, customizable online testing solution for business, training & educational assessments with Tests & Quizzes graded instantly, saving hours of paperwork!
+                    </p>
+                </div>
             </div>
 
             <div className='grid grid-cols-1 sm:grid-cols-4 mx-10 shadow-md p-5'>
-            {
-                topics.data.map(topic => <Topic
-                    key={topic.id}
-                    topic={topic}
+                {
+                    topics.data.map(topic => <Topic
+                        key={topic.id}
+                        topic={topic}
 
-                ></Topic>)
-            }
+                    ></Topic>)
+                }
 
             </div>
         </div>
